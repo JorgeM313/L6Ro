f62241529e74983c6689a246cd7f367aefd15dc1
JorgeM313 <42817785+JorgeM313@users.noreply.github.com>
2018-11-07 14:07:30 -0800

    Create L6Rou.cpp

---
+++ L6Rou.cpp
@@ -0,0 +1,26 @@
+// Jorge Martinez
+//CPSC 121 Lab 6
+//11/07/2018
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+struct Calculator
+{
+  long long LH Value;
+  long long * RH Value;
+  char lastOperator;
+};
+
+int main()
+{
+  Calculator problem = {0, 0, +}
+  cout << "Welcome to the Calculator program.\n";
+  cout << "Enter your expression:\n";
+  cin >>
+
+return 0;
+}
