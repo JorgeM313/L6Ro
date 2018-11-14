20adaf61ac3ddfb2b73ebe85a7f3e7361ce8b085
JorgeM313 <42817785+JorgeM313@users.noreply.github.com>
2018-11-14 14:19:42 -0800

    Update L6Rou.cpp

--- L6Rou.cpp
+++ L6Rou.cpp
@@ -8,19 +8,35 @@
 
 using namespace std;
 
-struct Calculator
+struct Calculator // Creating the struct provided
 {
-  long long LH Value;
-  long long * RH Value;
+  long long LHValue;
+  long long * RHValue;
   char lastOperator;
 };
+void processInput(Calculator * calc, string userInput) // Function that calculates the expression
+{
+  calc->LHValue
+}
 
 int main()
 {
-  Calculator problem = {0, 0, +}
+  Calculator problem = {0, 0, +};
+
+  problem.LHValue = 0;
+  problem.RHValue = new long long;
+  *(problem.RHValue) = 0;
+  problem.lastOperator = '+';
+
+  string userInput = 0;
   cout << "Welcome to the Calculator program.\n";
   cout << "Enter your expression:\n";
-  cin >>
+  cin.ignore();
+  getline(cin, userInput);
+  processInput(&problem, userInput);
 
 return 0;
 }
+
+
+// Note to self: Don't be afraid of the red letters. They are supposed to be like that
